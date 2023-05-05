0debd1c249be0ba0e3728ffe29836362dae12abd
Siroshun09 <siro.shun8@gmail.com>
2023-05-06 00:23:20 +0900

    build: add paper-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,14 @@ java {
 
 repositories {
     mavenCentral()
+
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
-
+    compileOnly("io.papermc.paper", "paper-api", "1.19.4-R0.1-SNAPSHOT")
 }
 
 tasks {
