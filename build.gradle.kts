3d61b79fd544ad0203ad1eb012ea4d822d314872
Siroshun09 <siro.shun8@gmail.com>
2023-05-06 00:23:20 +0900

    build: add paper-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,16 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     api("org.jetbrains:annotations:24.0.1")
+
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 
     testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
