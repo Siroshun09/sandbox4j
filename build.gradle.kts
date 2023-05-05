84de4102c32a2c801a69ef3f032ab743959387ec
Siroshun09 <siro.shun8@gmail.com>
2023-05-05 23:27:25 +0900

    build: setup gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java-library`
+}
+
+group = "com.github.siroshun09.sandbox4j"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
